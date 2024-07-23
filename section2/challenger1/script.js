7244f9205426686de0bff3b6f7531eca2bec86a0
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-07-23 20:19:05 -0300

    conclusão do challenger 1

---
+++ section2/challenger1/script.js
@@ -0,0 +1,23 @@
+
+// const massMark = 78
+// const heightMark = 1.69
+// const massJohn = 92
+// const heighJohn = 1.95
+
+const massMark = 95
+const heightMark = 1.88
+const massJohn = 85
+const heighJohn = 1.76
+
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heighJohn * heighJohn)
+
+console.log(BMIMark)
+console.log(BMIJohn)
+
+let markHigherBMI = BMIMark > BMIJohn
+
+console.log(markHigherBMI)
+
+
