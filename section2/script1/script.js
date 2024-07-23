b8079ea2c8061f997b3614f235e9dd3ed1b30451
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-07-23 18:39:32 -0300

    fim do script1 na secao2

---
+++ section2/script1/script.js
@@ -0,0 +1,11 @@
+// let js = "amazing";
+//       if(js === "amazing"){
+//         alert("JavaScript is FUN");
+//       }
+
+console.log(20 + 15 - 8 / 2)
+
+let firstName = "Jonas"
+
+console.log(firstName)
+
