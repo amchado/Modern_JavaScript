6cb4defa52cb855ed48de5201f51b8d8d65ba5c5
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-07-23 20:03:02 -0300

    fim do script 4 e 5

---
+++ section2/script4/assigments/script.js
@@ -0,0 +1,28 @@
+
+let isIsland = false;
+let language;
+
+language = "portugues"
+const currentYear = 2024
+const country = "Brasil"
+const populantion = 220000000
+
+isIsland = true;
+
+let halfPopulation = populantion / 2
+console.log(halfPopulation);
+halfPopulation++;
+console.log(halfPopulation);
+
+const finlandiaPopulation = 6000000
+console.log(populantion > finlandiaPopulation)
+
+let description = "O Brasil fica na America do Sul, e é um Pais com aproximadamente 220 milhões de pessoas"
+console.log(description)
+
+
+
+
+
+
+
---
+++ section2/script5/assigments/script.js
@@ -0,0 +1,30 @@
+
+let isIsland = false;
+let language;
+
+language = "portugues"
+const currentYear = 2024
+const continent = "America do Sul"
+const country = "Brasil"
+const populantion = 220
+
+isIsland = true;
+
+// let halfPopulation = populantion / 2
+// console.log(halfPopulation);
+// halfPopulation++;
+// console.log(halfPopulation);
+
+// const finlandiaPopulation = 6
+// console.log(populantion > finlandiaPopulation)
+
+let description = "O Brasil fica na America do Sul, e é um Pais com aproximadamente 220 milhões de pessoas"
+// console.log(description)
+
+description = `O ${country} fica na ${continent}, e é um Pais com aproximadamente ${populantion} milhões de pessoas `
+console.log(description)
+
+
+
+
+
