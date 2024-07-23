4f152011c7cda442826200b17049fff8e4efafe9
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-07-23 18:46:01 -0300

    fim scrip2

--- section2/script1/assigments/script.js
+++ section2/script1/assigments/script.js
@@ -1,7 +1,9 @@
-let country = "Brasil"
-let continent = "South America"
-let population = 200000000;
+let isIsland = false;
+let language;
 
-console.log(country)
-console.log(continent)
-console.log(population)
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
---
+++ section2/script2/assigments/script.js
@@ -0,0 +1,7 @@
+let country = "Brasil"
+let continent = "South America"
+let population = 200000000;
+
+console.log(country)
+console.log(continent)
+console.log(population)
