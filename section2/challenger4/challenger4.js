e02fabe321d02b1c8413adc09c01d23fb02671e1
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-07-26 13:09:45 -0300

    challenger4

--- section2/challenger4/challenger4.js
+++ section2/challenger4/challenger4.js
@@ -1,3 +1,7 @@
-const bill = 275
+const bill = 430
 
 let tip;
+
+tip = bill >= 50 && bill <= 300 ? `A conta deu ${bill}, a gorjeta foi ${(bill * 15/100)}, o valor total foi ${bill + (bill * 15/100)}.   ` : `o conta de ${bill}, a gorgeta foi ${bill * 20/100}, e o total foi de ${bill + (bill * 20/100)} `
+
+console.log(tip)
