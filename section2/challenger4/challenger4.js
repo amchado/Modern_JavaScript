31ecf0a54c5438cfc10be66c0f252bc383e45427
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-08-14 17:24:01 -0300

    mudança no texto

--- section2/challenger4/challenger4.js
+++ section2/challenger4/challenger4.js
@@ -2,6 +2,6 @@ const bill = 430
 
 let tip;
 
-tip = bill >= 50 && bill <= 300 ? `A conta deu ${bill}, a gorjeta foi ${(bill * 15/100)}, o valor total foi ${bill + (bill * 15/100)}.   ` : `o conta de ${bill}, a gorgeta foi ${bill * 20/100}, e o total foi de ${bill + (bill * 20/100)} `
+tip = bill >= 50 && bill <= 300 ? `A conta deu ${bill}, a gorjeta foi ${(bill * 15/100)}, o valor total foi ${bill + (bill * 15/100)}.   ` : `A conta de ${bill}, a gorgeta foi ${bill * 20/100}, e o total foi de ${bill + (bill * 20/100)} `
 
 console.log(tip)
