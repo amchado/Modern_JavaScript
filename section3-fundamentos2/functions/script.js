1923cdc1699a58b70b5820b14edc044f95820a71
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-08-20 17:25:12 -0300

    fundamentos

--- section3-fundamentos2/functions/script.js
+++ section3-fundamentos2/functions/script.js
@@ -11,23 +11,77 @@
 
 //Aula 2 
 
-function login() {
-    console.log('O login esta certo')
-}
+// function login() {
+//     console.log('O login esta certo')
+// }
 
-login()
+// login()
+
+// function fazedorDeSucos(maca,laranja) {
+//     const suco = `O suco contêm ${maca} maçães e ${laranja} laranjas.`
+//     return suco
+// }
+
+
+// const sucoMisto = fazedorDeSucos(3 , 2)
+// console.log(sucoMisto)
+
+// const sucoDeMaca = fazedorDeSucos(5, 0)
+// console.log(sucoDeMaca)
+
+
+
+
+// Aula 3 
+
+// function idade(anoDeNascimento) {
+//     return 2035 - anoDeNascimento
+// }
+
+// const idade1 = idade(2002)
+// console.log(idade1)
+
+// const idade2 = function(anoDeNascimento) {
+//     return 2035 - anoDeNascimento
+// }
+// const idade22 = idade2(2002)
+// console.log(idade22)
+
+
+
+
+//Aula 4 
+// Arrow functions
+
+// const idade = anoDeNascimento => 2024 - anoDeNascimento; 
+// const idade1 = idade(22)
+// console.log(idade1)
+
+// const anosRestantes = (anoDeNascimento, nome) => {
+//     const idade = 2024 - anoDeNascimento
+//     const aposentadoria = 65 - idade
+//     return `${nome} faltam ${aposentadoria} para a sua aposentadoria. ` 
+// }
+
+// const pessoa1 = anosRestantes(2002, 'Arthur')
+// console.log(pessoa1)
+
+//Aula 5
+//function calling other function
+
+function cortadorDeFrutas(fruta){
+    return fruta * 4
+}
 
 function fazedorDeSucos(maca,laranja) {
-    const suco = `O suco contêm ${maca} maçães e ${laranja} laranjas.`
+    const pedacoDeMaca = cortadorDeFrutas(maca)
+    const pedacoDeLaranja = cortadorDeFrutas(laranja) 
+
+    const suco = `O suco contêm ${pedacoDeMaca} pedaços de maçães e ${pedacoDeLaranja} pedaços de laranjas.`
     return suco
-}
+ }
 
-
-const sucoMisto = fazedorDeSucos(3 , 2)
-console.log(sucoMisto)
-
-const sucoDeMaca = fazedorDeSucos(5, 0)
-console.log(sucoDeMaca)
-
+ const suco1 = fazedorDeSucos(2,3)
+ console.log(suco1)
 
 
