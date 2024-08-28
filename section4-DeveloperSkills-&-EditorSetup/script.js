51fb3fc7c66615e83f14d9025d46d7844f8093c7
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-08-28 12:48:07 -0300

    Update script.js

--- section4-DeveloperSkills-&-EditorSetup/script.js
+++ section4-DeveloperSkills-&-EditorSetup/script.js
@@ -64,25 +64,73 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 
 // DEBUGGER 
-const calcTempAmplitudeBug = function(t1, t2){
-    const temps = t1.concat(t2)
-    console.log(temps)
+// const calcTempAmplitudeBug = function(t1, t2){
+//     const temps = t1.concat(t2)
+//     console.log(temps)
 
 
-    let max = 0
-    let min = 0
+//     let max = 0
+//     let min = 0
 
-    for(let i = 0; i < temps.length; i++){
-        const curTemp = temps[i]
+//     for(let i = 0; i < temps.length; i++){
+//         const curTemp = temps[i]
 
-        if(typeof curTemp !== 'number') continue;
+//         if(typeof curTemp !== 'number') continue;
 
-        if(curTemp > max){max = curTemp}
-        if(curTemp < min){min = curTemp}
+//         if(curTemp > max){max = curTemp}
+//         if(curTemp < min){min = curTemp}
+//     }
+//     console.log(max, min);
+//     return max - min
+// }
+
+// const amplitudeBug = calcTempAmplitudeBug([1,5,10],[6,2,15])
+// console.log(amplitudeBug);
+
+
+
+//Challenger 
+
+const arr1 = [17,21,23]
+const arr2 = [12,5, -5, 0, 4]
+
+// const printForecast = function(array){
+//     const forescast = []
+
+//     for(let i = 0 ; i < array.length; i++){
+//         const currentDay = i + 1
+//         if(currentDay === 1){
+//             forescast.push(`Dia ${currentDay} fez ${array[i]}° graus`)
+//         }else{
+//             forescast.push(`Dia ${currentDay} fez ${array[i]}° graus`)
+//         }
+        
+//     }
+//         return forescast
+// }
+
+// console.log(printForecast(arr1))
+
+
+
+// const printForecast2 = function(array){
+//     const forescast = []
+
+//     for(let i = 0 ; i < array.length; i++){
+//         forescast.push(`Dia ${i + 1} fez ${array[i]}° graus`)
+        
+//     }
+//         return forescast
+// }
+
+// console.log(printForecast2(arr2))
+
+const printForecast3 = function(array3){
+    let soma = '... '
+    for(let i = 0; i < array3.length; i++){
+        soma += `Dia ${i + 1} fez ${array3[i]}° graus ... `
     }
-    console.log(max, min);
-    return max - min
+    console.log(soma)
 }
 
-const amplitudeBug = calcTempAmplitudeBug([1,5,10],[6,2,15])
-console.log(amplitudeBug);
+printForecast3(arr1)
