4d03a9182250c7183e7d53477b68f1b3e95d2a0e
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-08-28 11:45:30 -0300

    Update script.js

--- section4-DeveloperSkills-&-EditorSetup/script.js
+++ section4-DeveloperSkills-&-EditorSetup/script.js
@@ -17,20 +17,20 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // - Find min value in temp array
 // - Subtract min from max (amplitude) and return it
 
-const calcTempAmplitude = function(temps){
-    let max = temps[0]
-    let min = temps[0]
-    for(let i = 0; i < temps.length; i++){
-        const curTemp = temps[i]
+// const calcTempAmplitude = function(temps){
+//     let max = temps[0]
+//     let min = temps[0]
+//     for(let i = 0; i < temps.length; i++){
+//         const curTemp = temps[i]
 
-        if(typeof curTemp !== 'number') continue;
+//         if(typeof curTemp !== 'number') continue;
 
-        if(curTemp > max){max = curTemp}
-        if(curTemp < min){min = curTemp}
-    }
-    console.log(max, min);
-    return max - min
-}
+//         if(curTemp > max){max = curTemp}
+//         if(curTemp < min){min = curTemp}
+//     }
+//     console.log(max, min);
+//     return max - min
+// }
 
 // const amplitude = calcTempAmplitude(temperatures)
 // console.log(amplitude);
@@ -39,13 +39,38 @@ const calcTempAmplitude = function(temps){
 
 
 
-const calcTempAmplitudeNew = function(t1, t2){
+// const calcTempAmplitudeNew = function(t1, t2){
+//     const temps = t1.concat(t2)
+//     console.log(temps)
+
+
+//     let max = temps[0]
+//     let min = temps[0]
+
+//     for(let i = 0; i < temps.length; i++){
+//         const curTemp = temps[i]
+
+//         if(typeof curTemp !== 'number') continue;
+
+//         if(curTemp > max){max = curTemp}
+//         if(curTemp < min){min = curTemp}
+//     }
+//     console.log(max, min);
+//     return max - min
+// }
+
+// const amplitudeNew = calcTempAmplitudeNew([1,5,10],[-50,2,15])
+// console.log(amplitudeNew);
+
+
+// DEBUGGER 
+const calcTempAmplitudeBug = function(t1, t2){
     const temps = t1.concat(t2)
     console.log(temps)
 
 
-    let max = temps[0]
-    let min = temps[0]
+    let max = 0
+    let min = 0
 
     for(let i = 0; i < temps.length; i++){
         const curTemp = temps[i]
@@ -59,5 +84,5 @@ const calcTempAmplitudeNew = function(t1, t2){
     return max - min
 }
 
-const amplitudeNew = calcTempAmplitudeNew([1,5,10],[-50,2,15])
-console.log(amplitudeNew);
+const amplitudeBug = calcTempAmplitudeBug([1,5,10],[6,2,15])
+console.log(amplitudeBug);
