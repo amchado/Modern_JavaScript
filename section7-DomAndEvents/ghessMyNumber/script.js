882e7b30de524245aac2fdb5c7561e971caea614
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-09-03 19:48:12 -0300

    reduzi o codigo duplicado na função

--- section7-DomAndEvents/ghessMyNumber/script.js
+++ section7-DomAndEvents/ghessMyNumber/script.js
@@ -1,17 +1,5 @@
 'use strict';
 
-/*
-document.querySelector('.message').textContent = '🎉 Correct number!'
-console.log(document.querySelector('.message').textContent)
-
-document.querySelector('.score').textContent = 10
-console.log(document.querySelector('.score').textContent);
-
-document.querySelector('.number').textContent = 15
-
-document.querySelector('.guess').value = 5
-console.log(document.querySelector('.guess').value)
-*/
 
 let secretNumber = Math.trunc(Math.random()*20) + 1
 let score = 20
@@ -36,25 +24,38 @@ document.querySelector('.check').addEventListener('click', function(){
 
         document.querySelector('body').style.backgroundColor = '#60b347'
         document.querySelector('.number').style.width = '30rem'
-    }else if(guess > secretNumber){
+
+    
+    }else if(guess !== secretNumber){
         if(score > 1){
-            document.querySelector('.message').textContent = '📈 Too high'
+            document.querySelector('.message').textContent = guess > secretNumber ?'📈 Too high' : '📉 Too low'
             score--
             document.querySelector('.score').textContent = score
         }else{
             document.querySelector('.message').textContent = '☠ You lose the game'
             document.querySelector('.score').textContent = 0
         }
-    }else if(guess < secretNumber){
-       if(score > 1){
-        document.querySelector('.message').textContent = '📉 Too low'
-        score--
-        document.querySelector('.score').textContent = score
-       }else{
-        document.querySelector('.message').textContent = '☠ You lose the game'
-        document.querySelector('.score').textContent = 0
-       }
     }
+
+    // } else if(guess > secretNumber){
+    //     if(score > 1){
+    //         document.querySelector('.message').textContent = '📈 Too high'
+    //         score--
+    //         document.querySelector('.score').textContent = score
+    //     }else{
+    //         document.querySelector('.message').textContent = '☠ You lose the game'
+    //         document.querySelector('.score').textContent = 0
+    //     }
+    // }else if(guess < secretNumber){
+    //    if(score > 1){
+    //     document.querySelector('.message').textContent = '📉 Too low'
+    //     score--
+    //     document.querySelector('.score').textContent = score
+    //    }else{
+    //     document.querySelector('.message').textContent = '☠ You lose the game'
+    //     document.querySelector('.score').textContent = 0
+    //    }
+    // }   
 
 
 })
