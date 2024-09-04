c26e7caebc44d3f5426e7b9bd2c491953ed6a6b6
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-09-04 20:54:19 -0300

    projeto modal finalizado 😜

--- section7-DomAndEvents/modal/script.js
+++ section7-DomAndEvents/modal/script.js
@@ -3,13 +3,30 @@
 
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
-const btnCloseModal = document.querySelector('.close-model')
+const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
+const openModal =  function(){
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+    
+     
+} 
+const closeModel = function(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
 
 for(let i = 0 ; i < btnsOpenModal.length ; i++){
-    btnsOpenModal[i].addEventListener('click', function(){
-        modal.classList.remove('hidden')
-        console.log('botton click')
-    })
+    btnsOpenModal[i].addEventListener('click', openModal)
 }
+
+btnCloseModal.addEventListener('click', closeModel)
+overlay.addEventListener('click', closeModel)
+
+document.addEventListener('keydown', function(e){
+    
+    if(e.key === 'Escape' && !modal.classList.contains('hidden') || e.key === 'Backspace' && !modal.classList.contains('hidden')){
+        closeModel()
+    }
+})
