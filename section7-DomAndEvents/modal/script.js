865e01c1622b0713a6c03e20c0c1e87b6d176a69
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-09-04 19:42:42 -0300

    .

--- section7-DomAndEvents/modal/script.js
+++ section7-DomAndEvents/modal/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-model')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+
+for(let i = 0 ; i < btnsOpenModal.length ; i++){
+    btnsOpenModal[i].addEventListener('click', function(){
+        modal.classList.remove('hidden')
+        console.log('botton click')
+    })
+}
