2df239a956665006851696bdb1681e43ede39a07
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-09-10 20:51:21 -0300

    1/2 do projeto

--- section7-DomAndEvents/pig-game/script.js
+++ section7-DomAndEvents/pig-game/script.js
@@ -1,12 +1,51 @@
 'use strict';
 
-const roll = Math.trunc(Math.random() * 6 ) + 1 
-
+//selecting elements
+const player0El = document.querySelector('.player--0')
+const player1El = document.querySelector('.player--1')
 const score0El = document.querySelector('#score--0')
 const score1El = document.getElementById('score--1')
-const diceEl = document.querySelector('.dice') 
+const current0El = document.getElementById('current--0')
+const current1El = document.getElementById('current--1')
 
+const diceEl = document.querySelector('.dice') 
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold')
+
+
+//Start conditions
+const scores = [0, 0]
+let activePlayer = 0
 score0El.textContent = 0
 score1El.textContent = 0
 diceEl.classList.add('hidden')
+
+let currentScore = 0
+
+//rolling dice functionality
+btnRoll.addEventListener('click', function(){
+    //1. gerenating a random dice roll
+    let dice = Math.trunc(Math.random() * 6 ) + 1 
+    console.log(dice)
+
+    //2. display dice 
+    diceEl.classList.remove('hidden')
+    diceEl.src = `dice-${dice}.png`
+
+    //3. check the rolled 1
+    if(dice !== 1){
+        // add dice to current score
+        currentScore += dice
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore
+    }else{
+        document.getElementById(`current--${activePlayer}`).textContent = 0
+        currentScore = 0
+        activePlayer = activePlayer === 0 ? 1 : 0
+        player0El.classList.toggle('player--active')
+        player1El.classList.toggle('player--active')
+    }
+
+
+})
 
