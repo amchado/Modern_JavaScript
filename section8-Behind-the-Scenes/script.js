1247ee81972b84944c6bf270c23584b39409f6cc
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-09-17 15:37:06 -0300

    Update script.js

--- section8-Behind-the-Scenes/script.js
+++ section8-Behind-the-Scenes/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1986 && birthYear <= 1999) {
+      const str = `Oh, and you are a millenial, ${firstName}`;
+
+      console.log(str);
+    }
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Arthur';
+calcAge(1989);
