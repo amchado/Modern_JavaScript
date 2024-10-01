84708de41d38a667f34773d5b69c8d4819315772
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-10-01 12:08:59 -0300

    optionalChaining

--- section9-Data-Structures-Operators/challengers/cl8-enhancedObjcts/script.js
+++ section9-Data-Structures-Operators/challengers/cl8-enhancedObjcts/script.js
@@ -226,6 +226,28 @@ const books = [
 ];
 
 //
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+
+// Do the rest
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1],
+};
+
+console.log(newBook);
+
+const pages = 880;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  pages,
+};
 
 /*
 //
