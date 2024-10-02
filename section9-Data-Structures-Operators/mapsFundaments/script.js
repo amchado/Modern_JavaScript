2b5898c5fd6ab8ee26145282d32b129a6419f6d4
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-10-02 19:31:45 -0300

    Maps fundaments

--- section9-Data-Structures-Operators/mapsFundaments/script.js
+++ section9-Data-Structures-Operators/mapsFundaments/script.js
@@ -52,6 +52,34 @@ const restaurant = {
   },
 };
 
+const rest = new Map();
+rest.set('name', 'Italianinho massas');
+rest.set(1, 'Rua francisca, RJ');
+rest.set(2, 'Avenida Braza, BXD');
+console.log(rest);
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :)')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(false));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+rest.set(document.querySelector('h1'), 'Heading');
+// rest.clear()
+rest.set([1, 2], 'test');
+console.log(rest);
+console.log(rest.size);
+
 /*
 const orderSets = new Set([
   'pizza',
