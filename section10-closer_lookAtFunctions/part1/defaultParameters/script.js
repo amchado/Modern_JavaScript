1a8d6bff90123ed4552a4c0d3f635dc5c46a01e9
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2024-10-08 12:00:47 -0300

    Default parameters

---
+++ section10-closer_lookAtFunctions/part1/defaultParameters/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNumber,
+  numPassengers = 1,
+  price = 100 * numPassengers
+) {
+  //ES5
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 1;
+
+  const booking = {
+    flightNumber,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(bookings);
+};
+
+createBooking('Ls50');
+createBooking('Ls100', 2);
+createBooking('BG30', 3, 50);
+createBooking('LT02', undefined, 20);
+createBooking('AL50', 10, undefined);
