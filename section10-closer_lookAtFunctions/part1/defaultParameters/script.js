99536ef95a0aede0861a8b39f86f359bd0307915
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2025-07-30 15:16:19 -0300

    Update script.js

--- section10-closer_lookAtFunctions/part1/defaultParameters/script.js
+++ section10-closer_lookAtFunctions/part1/defaultParameters/script.js
@@ -2,7 +2,8 @@
 
 //////////////////////////////////////////////////////////
 // Default Parameters
-const bookings = [];
+
+/*const bookings = [];
 
 const createBooking = function (
   flightNumber,
@@ -27,3 +28,34 @@ createBooking('Ls100', 2);
 createBooking('BG30', 3, 50);
 createBooking('LT02', undefined, 20);
 createBooking('AL50', 10, undefined);
+*/
+//How Passing Arguments Works: Value vs. Reference
+
+const flight = 'AR1408';
+const arthur = {
+  name: 'Arthur Lucas',
+  passport: 21995126152111,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'BR0111';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 21995126152111) {
+    alert('Voo checado, tudo certo');
+  } else {
+    alert('Erro no passaporte');
+  }
+};
+
+// checkIn(flight, arthur);
+// console.log(flight);
+// console.log(arthur);
+
+const newPassport = function (person) {
+  person.passport = 1000100001000;
+};
+
+newPassport(arthur);
+checkIn(flight, arthur);
+console.log(arthur);
