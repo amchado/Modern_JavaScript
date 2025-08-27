7f74278e917540dec576a1cc9785fc4474bd0743
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2025-08-27 18:54:26 -0300

    Update script.js

--- section10-closer_lookAtFunctions/part1/defaultParameters/script.js
+++ section10-closer_lookAtFunctions/part1/defaultParameters/script.js
@@ -204,3 +204,50 @@ const addTAX = (rate, value) => value + value * rate;
 (() => {
   console.log(`Isso tambem vai ser executado somente uma vez`);
 })();
+
+///////////////////////////////////////
+// Closures
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+///////////////////////////////////////
+// More Closure Examples
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
