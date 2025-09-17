ee84af1598fe12b4ca8e1a8f4fae77f318b08c5b
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2025-09-17 14:47:36 -0300

    Update script.js

--- section12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ section12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -80,7 +80,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 // Functions\
-const formatMovementDate = function (date) {
+const formatMovementDate = function (date, locale) {
   const calcDaysPassed = (date1, date2) =>
     Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
@@ -90,10 +90,13 @@ const formatMovementDate = function (date) {
   if (daysPassed === 0) return 'Today ';
   if (daysPassed === 1) return 'Yesterday ';
   if (daysPassed < 7) return `${daysPassed} days ago`;
-  const day = `${date.getDate()}`.padStart(2, '0');
-  const year = date.getFullYear();
-  const mounth = `${date.getMonth() + 1}`.padStart(2, '0');
-  return `${day}/${mounth}/${year}`;
+
+  return new Intl.DateTimeFormat(locale).format(date);
+
+  // const day = `${date.getDate()}`.padStart(2, '0');
+  // const year = date.getFullYear();
+  // const mounth = `${date.getMonth() + 1}`.padStart(2, '0');
+  // return `${day}/${mounth}/${year}`;
 };
 
 const displayMovements = function (acc, sort = false) {
@@ -107,7 +110,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const displayDate = formatMovementDate(date);
+    const displayDate = formatMovementDate(date, acc.locale);
 
     const html = `
       <div class="movements__row">
@@ -197,13 +200,28 @@ btnLogin.addEventListener('click', function (e) {
     containerApp.style.opacity = 100;
 
     /////// Create current date and time
+    ///////Date API
     const now = new Date();
-    const date = `${now.getDate()}`.padStart(2, '0');
-    const year = now.getFullYear();
-    const mounth = `${now.getMonth() + 1}`.padStart(2, '0');
-    const hrs = `${now.getHours()}`.padStart(2, '0');
-    const minutes = `${now.getMinutes()}`.padStart(2, '0 ');
-    labelDate.textContent = `${date}/${mounth}/${year}, ${hrs}:${minutes}`;
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'numeric',
+      year: 'numeric',
+      // weekday: 'long',
+    };
+    // const locale = navigator.language;
+
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
+    // const date = `${now.getDate()}`.padStart(2, '0');
+    // const year = now.getFullYear();
+    // const mounth = `${now.getMonth() + 1}`.padStart(2, '0');
+    // const hrs = `${now.getHours()}`.padStart(2, '0');
+    // const minutes = `${now.getMinutes()}`.padStart(2, '0 ');
+    // labelDate.textContent = `${date}/${mounth}/${year}, ${hrs}:${minutes}`;
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
