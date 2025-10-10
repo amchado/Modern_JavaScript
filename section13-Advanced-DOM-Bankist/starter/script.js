1ff77408ff9b3612fbe8a5c43c687414a4128bbe
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2025-10-10 16:53:58 -0300

    Update script.js

--- section13-Advanced-DOM-Bankist/starter/script.js
+++ section13-Advanced-DOM-Bankist/starter/script.js
@@ -62,6 +62,37 @@ console.log(getComputedStyle(message).height);
 
 document.documentElement.style.setProperty('--color-primary', 'orangered');
 
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// Don't use
+logo.clasName = 'jonas';
+
 //////////////////////////////////////////
 /*
 // Selecting, Creating, and Deleting Elements
