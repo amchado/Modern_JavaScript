25cbff2f1f4833b0347c663e38361b415c006bd9
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2025-10-16 16:14:23 -0300

    Update script.js

--- section13-Advanced-DOM-Bankist/starter/script.js
+++ section13-Advanced-DOM-Bankist/starter/script.js
@@ -134,6 +134,16 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+////Sticy navegation
+const initialCords = section1.getBoundingClientRect();
+window.addEventListener('scroll', function (e) {
+  if (window.scrollY > initialCords.top) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+});
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //// botao Cookies
 // const headar = document.querySelector('.header');
