883453fd0343e96b882db8bf84a25d1eb7b6d3d4
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2025-10-16 18:58:34 -0300

    Update script.js

--- section13-Advanced-DOM-Bankist/starter/script.js
+++ section13-Advanced-DOM-Bankist/starter/script.js
@@ -185,11 +185,12 @@ headerObserver.observe(header);
 const allSections = document.querySelectorAll('.section');
 
 const revelSection = function (entries, observer) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
 
-  entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target);
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
 };
 
 const sectionObserver = new IntersectionObserver(revelSection, {
