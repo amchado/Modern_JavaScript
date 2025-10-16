f36de9dbe556029a387469a89cfaca11c5074edc
Arthur Lucas <115301563+amchado@users.noreply.github.com>
2025-10-16 15:34:53 -0300

    Primeira versao do hover no nav

--- section13-Advanced-DOM-Bankist/starter/script.js
+++ section13-Advanced-DOM-Bankist/starter/script.js
@@ -6,6 +6,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 ///////////////////////////////////////
 // Modal window
@@ -90,9 +94,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 /////////////////tabbed component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
@@ -111,6 +112,24 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+/////Menu fade animation
+nav.addEventListener('mouseover', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = 0.5;
+      }
+      logo.style.opacity = 0.5;
+    });
+  }
+});
+
+nav.addEventListener('mouseout', function (e) {});
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //// botao Cookies
